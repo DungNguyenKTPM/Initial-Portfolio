a0e4b962ff25b4d26cbe20625b579fe17e6d8a6f
songocuxi <105623544+songocuxi@users.noreply.github.com>
2024-11-30 23:45:35 +0700

    Updates

---
+++ assets/Layout-CDKC43R3.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-YLWWSWRk.js";import{P as s}from"./Footer-Ce2tcr4j.js";const r="_containerLayout_12fks_1",a={containerLayout:r};function e({children:n}){const{containerLayout:t}=a;return o.jsx("main",{children:o.jsx("div",{className:t,children:n})})}e.propTypes={children:s.node.isRequired};export{e as M};
