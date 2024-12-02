65b0ba830f752e0ed6ca7267f3d81f0edcd602c6
songocuxi <105623544+songocuxi@users.noreply.github.com>
2024-12-02 16:18:37 +0700

    Updates

---
+++ assets/Layout-CetzhdMR.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B3zO9XZz.js";import{P as s}from"./Footer-BL1VpNjQ.js";const r="_containerLayout_12fks_1",a={containerLayout:r};function e({children:n}){const{containerLayout:t}=a;return o.jsx("main",{children:o.jsx("div",{className:t,children:n})})}e.propTypes={children:s.node.isRequired};export{e as M};
