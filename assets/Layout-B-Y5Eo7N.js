8c6c726ff1a7a5c827c987c6da598dd7084b4a8c
songocuxi <105623544+songocuxi@users.noreply.github.com>
2024-12-02 15:42:42 +0700

    Updates

--- assets/Layout-B-Y5Eo7N.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BIFE3DhV.js";import{P as s}from"./Footer-BVV4BlxN.js";const r="_containerLayout_12fks_1",a={containerLayout:r};function e({children:n}){const{containerLayout:t}=a;return o.jsx("main",{children:o.jsx("div",{className:t,children:n})})}e.propTypes={children:s.node.isRequired};export{e as M};
---
+++ assets/Layout-CW0uEDs6.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B0T24qg9.js";import{P as s}from"./Footer-_mUZYQW_.js";const a="_containerLayout_12fks_1",e={containerLayout:a};function r({children:n}){const{containerLayout:t}=e;return console.log("Layout log"),o.jsx("main",{children:o.jsx("div",{className:t,children:n})})}r.propTypes={children:s.node.isRequired};export{r as M};
